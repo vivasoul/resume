d5a62207e8d6e00dcd412022717981307854aea2
vivasoul@naver.com <vivasoul@naver.com>
2022-08-24 23:01:50 +0900

    update: Break down skill components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,34 +1,65 @@
-import React, { Component } from "react";
-import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import React, { Component, useState } from "react";
+import { Container, Stack, Row, Col,
+         Card, Button, Collapse, Badge
+      } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faAnglesUp, faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 import { faStar as frStar } from '@fortawesome/free-regular-svg-icons';
 
 function SkillLevel({stars}) {
   var arr = [0,1,2,3,4];
 
   return (
-    <>
+    <div>
       {arr.map(e => <FontAwesomeIcon key={"star_"+e} icon={e < stars ? faStar : frStar} color="#1C89ED"/>)}
+    </div>
+  )
+}
+
+function SkillTitle({text, role}) {
+  return (
+    <div className="justify-space-block">
+      <div>{text}</div>
+      <div><Badge pill bg="warning">{role}</Badge></div>
+    </div>
+  )
+}
+
+function SkillDetail({stars}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="justify-space-block">
+        <SkillLevel stars={stars}/>
+        <Button size="sm"
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        ><FontAwesomeIcon icon={open ? faAnglesUp : faAnglesDown}/></Button>
+      </div>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          JAVA<br/>
+          디테일입니다<br/>
+          하하
+        </div>
+      </Collapse>
     </>
   )
 }
 
-function SkillCard(props){
+function SkillCard({stars}){
   return (
     <Card style={{width:"100%"}}>
       <Card.Body>
-        <Card.Title>JAVA</Card.Title>
-        <Card.Text>
-          <SkillLevel stars={2}/>
+        <Card.Title><SkillTitle text="JAVA" role="server" /></Card.Title>
+        <Card.Text as="div">
+          <SkillDetail stars={stars}/>
         </Card.Text>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
 export default class Skills extends Component {
@@ -39,12 +70,12 @@ export default class Skills extends Component {
         <Container>
           <Stack gap={3}>
             <Row>
-              <Col><SkillCard/></Col>
-              <Col><SkillCard/></Col>
+              <Col><SkillCard stars={2}/></Col>
+              <Col><SkillCard stars={3}/></Col>
             </Row>
             <Row>
-              <Col><SkillCard/></Col>
-              <Col><SkillCard/></Col>
+              <Col><SkillCard stars={4}/></Col>
+              <Col><SkillCard stars={5}/></Col>
             </Row>
           </Stack>
         </Container>
